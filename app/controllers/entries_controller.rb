6a8714c7520730e8049c132b4bffd3016567f19a
sydneygold <sydney.goldberg.com>
2020-11-13 14:03:29 -0700

    delete and update resources created

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,5 @@
 class EntriesController < ApplicationController
+    before_action :find_entry, only: [:update, :destroy]
     def index
         @entries = Entry.all 
 
@@ -6,20 +7,31 @@ class EntriesController < ApplicationController
     end
 
     def create
-        @entry = Entry.create(
-            title: params[:title],
-            content: params[:content],
-            image: params[:image]
-        )
+        @entry = Entry.create(entry_params)
 
         render json: @entry, status: :created
     end
 
-    def destroy
-        @entry = Entry.find(params:[:id])
+    def update 
 
+        @entry.update(entry_params)
+
+        render json: @entry
+    end
+
+    def destroy
         @entry.destroy
 
         render status: :no_content
+    end
+
+    private 
+
+    def find_entry
+        @entry = Entry.find(params[:id])
+    end
+
+    def entry_params
+        params.require(:entry).permit(:title, :content, :image)
     end
 end
